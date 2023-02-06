6f4c04c9f53c07fea8bf7c8e1b5af7eec2f3d090
Lukas Koch <lukas.koch@mailbox.org>
2023-02-06 19:47:01 +0100

    Fix CI. (#49)
    * Verbose pip.
    
    * Fix build.

--- src/remu/__init__.py
+++ src/remu/__init__.py
@@ -4,3 +4,8 @@ A framework for likelihood calculations and hypothesis testing using binned
 events and response matrices.
 
 """
+
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = None
